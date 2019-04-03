e14f3a5619e061f05c5fdf778f09ef04606287fd
mikinty <myou@andrew.cmu.edu>
2019-04-02 22:35:40 -0400

    led test

--- src/arduino/jacket_controls/jacket_controls.ino
+++ src/arduino/jacket_controls/jacket_controls.ino
@@ -13,7 +13,11 @@
 #define NUM_LEDS    300
 /** @brief number of LEDs that wrap around the turns */
 #define NUM_LEDS_SKIP 3
-#define LED_BRIGHTNESS 255
+#define LED_BRIGHTNESS_BRAKE 30
+
+#define TURN_R 255
+#define TURN_G 191
+#define TURN_B 0
 
 #define HEIGHT 16
 #define WIDTH  16
@@ -40,6 +44,24 @@ const uint16_t
     0B0010000000000100,
     0B0100000000000010,
     0B1000000000000001 
+  }, 
+  left_turn[] = { 
+    0B1000000000000001, 
+    0B0100000000000010,
+    0B0010000000000100,
+    0B0001000000001000,
+    0B0000100000010000,
+    0B0000010000100000,
+    0B0000001001000000,
+    0B0000000110000000,
+    0B1000000000000001, 
+    0B0100000000000010,
+    0B0010000000000100,
+    0B0001000000001000,
+    0B0000100000010000,
+    0B0000010000100000,
+    0B0000001001000000,
+    0B0000000110000000
   };
 
 CRGB leds[NUM_LEDS];
@@ -48,31 +70,25 @@ bool screen_on;
 /**
  * TODO: give color options...for now it's going to be red
  */
-void show_matrix (uint16_t M[]) {
+void show_matrix (uint16_t M[], int r, int g, int b) {
   int curr_led = 0;
   
   for (int col = 0; col < WIDTH; col++) {
-    Serial.print("CURR COL ");
-    Serial.println(col);
     uint16_t curr_bitvec = M[col];
     uint16_t mask = 0B0000000000000001;
     
     for (int row = 0; row < HEIGHT; row++) {
       if (curr_bitvec & mask == 1) {
-        leds[curr_led] = CRGB(LED_BRIGHTNESS, 0, 0);
-        Serial.print(curr_led);
-        Serial.print('|');
-        Serial.println(1);
+        leds[curr_led] = CRGB(r, g, b);
       } else {
         leds[curr_led] = CRGB(0, 0, 0);
-        Serial.println(curr_led);
       }
 
       curr_led++;
       curr_bitvec = curr_bitvec >> 1;
-      Serial.println(curr_bitvec);
     }
 
+    // Skip LEDs that loop around at the corners
     for (int i = 0; i < NUM_LEDS_SKIP; i++) {
       if (curr_led < 300)
         leds[curr_led] = CRGB(0, 0, 0);
@@ -81,20 +97,10 @@ void show_matrix (uint16_t M[]) {
   }
 }
 
-void display_signal (int type) {
-  switch (type) {
-    case 0:
-      show_matrix(right_turn);
-      break;
-  }
-  FastLED.show();
-}
-
-void on_screen() {
+void on_screen(int r, int g, int b) {
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(10, 0, 10);
+    leds[i] = CRGB(r, g, b);
   }
-  FastLED.show();
 }
 
 
@@ -102,6 +108,31 @@ void clear_screen() {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB(0, 0, 0);
   }
+  FastLED.show();
+}
+
+/**
+ * Selects the matrix display image based on the user settings
+ */
+void display_signal (int type) {
+  switch (type) {
+    case 0:
+      show_matrix(right_turn, TURN_R, TURN_G, TURN_B);
+      break;
+
+    case 1:
+      show_matrix(left_turn, TURN_R, TURN_G, TURN_B);
+      break;
+
+    case 2:
+      on_screen(LED_BRIGHTNESS_BRAKE, 0, 0);
+      break;
+
+    // HAZARD
+
+    // BRAKE AND TURN AT THE SAME TIME
+  }
+  
   FastLED.show();
 }
 
@@ -122,11 +153,12 @@ void setup() {
 void loop() {  
   int data;
 
-  clear_screen();
-  delay(1000);
-  // on_screen();
-  display_signal(0);
-  delay(1000);
+  for (int i = 0; i < 3; i++) {
+    clear_screen();
+    delay(1000);
+    display_signal(i);
+    delay(1000);  
+  }
 
   /*
   if(Serial.available() > 0) {
